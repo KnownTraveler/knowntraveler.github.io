7ed8b8229962e511ad143aaf5ec1bfc956bb9b2d
Brian Hooper <knowntraveler73@gmail.com>
2019-07-28 21:48:31 -0500

    Deploying Latest Site Build -

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+JEKYLL_VERSION=3.8.6
+CURRENT_DATE=`date +"%Y-%m-%d %T"`
+
+echo "Running Docker Build Process"
+docker run --rm --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll build
+
+echo "Committing Latest Build to github.com/knowntraveler/knowntraveler-web"
+git add .
+git commit -m "Committing Latest Site Build - $DATE"
+git push -u origin master
+
+echo "Deploying Latest Build to github.com/knowntraveler/knowntraveler.github.io"
+cd ./_site
+git add .
+git commit -m "Deploying Latest Site Build - $DATE"
+git push -u origin master
+
